a6ceab4150b595d31da3232aa59245e5fa5e0837
faisalabdulkadir <wizardfaisal@gmail.com>
2026-03-16 19:43:23 +0100

    minor updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import ExpenseList from "./pages/ExpenseList";
 import ExpenseTrackerForm from "./pages/ExpenseTrackerForm";
+import Summary from "./pages/Summary";
 
 function App() {
   return (
     <section className=" flex justify-center items-center">
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2">
         <div className="p-4">
-         <ExpenseTrackerForm />
+          <ExpenseTrackerForm />
+        </div>
+         <div className="p-4">
+          <Summary />
         </div>
         <div className="p-4">
-
-        <ExpenseList />
+          <ExpenseList />
         </div>
       </div>
     </section>

2e3cb823119ddad0ea42eaf0f25b751452a156fa
faisalabdulkadir <wizardfaisal@gmail.com>
2026-03-19 18:55:22 +0100

    implemented edit feature

--- src/pages/Expense.tsx
+++ src/pages/Expense.tsx
@@ -1,6 +1,7 @@
 import {
   CardBody,
   Chip,
+  Input,
   Select,
   SelectItem,
   Table,
@@ -15,8 +16,12 @@ import { ToggleIcon } from "../components/ToggleIcon";
 import { EditIcon } from "../components/EditIcon";
 import { DeleteIcon } from "../components/DeleteIcon";
 import type { Expense, StatusColour } from "../types/types";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { categories, statuses } from "../data/data";
+import { useDispatch } from "react-redux";
+import { deleteExpense, updateExpense } from "../features/expense/expenseSlice";
+import { SaveIcon } from "../components/SaveIcon ";
+import { CancelIcon } from "../components/CancelIcon ";
 
 type ExpensesProp = {
   columns: Record<string, string>[];
@@ -29,18 +34,62 @@ export const Expenses = ({
   expenses,
   statusColorMap,
 }: ExpensesProp) => {
+  const [editID, setEditID] = useState<string>("");
+  const [editExpense, setEditExpense] = useState<Expense | null>(null);
+
+  const dispatch = useDispatch();
+  const handleEditExpense = (expense: Expense) => {
+    console.log({ expense });
+    setEditID(expense.id as string);
+    setEditExpense(expense);
+  };
+
+  const handleChange = (value: string) => {
+    setEditExpense((prev) => {
+      if (!prev) return null;
+      return { ...prev, title: value };
+    });
+  };
+
+  const handleDeleteExpense = (id: string) => {
+    dispatch(deleteExpense({ id }));
+  };
+
+  const handleUpdateExpense = (expense: Expense) => {
+    dispatch(
+      updateExpense({
+        id: expense.id as string,
+        title: editExpense?.title ?? "",
+      }),
+    );
+    setEditExpense(null);
+    setEditID("");
+  };
+
+  const handleCancelEdit = () => {
+    setEditExpense(null);
+    setEditID("");
+  };
   const renderCell = useCallback(
     (expenses: Expense, columnKey: string | number) => {
       const cellValue = expenses[columnKey as keyof Expense];
+      const isEditing = editID === expenses.id;
+      console.log({ editdetails: expenses.id, editID });
 
       switch (columnKey) {
         case "title":
           return (
             <div className="flex flex-col">
-              <p className="text-bold text-sm capitalize">{cellValue}</p>
-              {/* <p className="text-bold text-sm capitalize text-default-400">
-              {user.team}
-            </p> */}
+              {isEditing ? (
+                <Input
+                  name="title"
+                  placeholder="Enter your title"
+                  value={editExpense?.title}
+                  onValueChange={handleChange}
+                />
+              ) : (
+                <p className="text-bold text-sm capitalize">{cellValue}</p>
+              )}
             </div>
           );
         case "status":
@@ -62,23 +111,52 @@ export const Expenses = ({
                   <ToggleIcon expense={expenses} />
                 </span>
               </Tooltip>
-              <Tooltip content="Edit expense">
-                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                  <EditIcon />
-                </span>
-              </Tooltip>
-              <Tooltip color="danger" content="Delete expense">
-                <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                  <DeleteIcon />
-                </span>
-              </Tooltip>
+              {isEditing ? (
+                <>
+                  <Tooltip color="success" content="Save expense">
+                    <span
+                      className="text-lg text-success cursor-pointer active:opacity-50"
+                      onClick={() => handleUpdateExpense(expenses)}
+                    >
+                      <SaveIcon />
+                    </span>
+                  </Tooltip>
+                  <Tooltip color="danger" content="Cancel expense">
+                    <span
+                      className="text-lg text-danger cursor-pointer active:opacity-50"
+                      onClick={handleCancelEdit}
+                    >
+                      <CancelIcon />
+                    </span>
+                  </Tooltip>
+                </>
+              ) : (
+                <>
+                  <Tooltip content="Edit expense">
+                    <span
+                      className="text-lg text-default-400 cursor-pointer active:opacity-50"
+                      onClick={() => handleEditExpense(expenses)}
+                    >
+                      <EditIcon />
+                    </span>
+                  </Tooltip>
+                  <Tooltip color="danger" content="Delete expense">
+                    <span
+                      className="text-lg text-danger cursor-pointer active:opacity-50"
+                      onClick={() => handleDeleteExpense(expenses.id as string)}
+                    >
+                      <DeleteIcon />
+                    </span>
+                  </Tooltip>
+                </>
+              )}
             </div>
           );
         default:
           return cellValue;
       }
     },
-    [],
+    [editID, editExpense],
   );
 
   return (
@@ -122,14 +200,21 @@ export const Expenses = ({
               </TableColumn>
             )}
           </TableHeader>
-          <TableBody items={expenses}>
-            {(item) => (
+          <TableBody>
+            {/* {(item) => (
               <TableRow key={item.id}>
                 {(columnKey) => (
                   <TableCell>{renderCell(item, columnKey)}</TableCell>
                 )}
               </TableRow>
-            )}
+            )} */}
+            {expenses.map((exp) => (
+              <TableRow key={exp.id}>
+                {(columnKey) => (
+                  <TableCell>{renderCell(exp, columnKey)}</TableCell>
+                )}
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </CardBody>
